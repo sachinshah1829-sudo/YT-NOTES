3eafbbed432f4059641675a13d4c948d6eeeaa25
sachinshah1829-sudo <sachinshah1829@gmail.com>
2026-04-09 21:54:09 +0530

    Update Firebase configuration for new project
    Updated Firebase configuration with new project details.

--- js/firebase.js
+++ js/firebase.js
@@ -4,13 +4,13 @@ import { getAuth }        from "https://www.gstatic.com/firebasejs/10.12.2/fireb
 import { getFirestore }   from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyAt2VZJi6nXCMunuQt_qOarq3NjYhabQzI",
-  authDomain:        "yt-notes-11cfa.firebaseapp.com",
-  projectId:         "yt-notes-11cfa",
-  storageBucket:     "yt-notes-11cfa.firebasestorage.app",
-  messagingSenderId: "458309083115",
-  appId:             "1:458309083115:web:92f10291b5616faadd83ae",
-  measurementId:     "G-0VE36L83JS"
+  apiKey: "AIzaSyD086hlwt6vqIvqvk-maOIftIjGRKUWJgg",
+  authDomain: "yt-notes-91623.firebaseapp.com",
+  projectId: "yt-notes-91623",
+  storageBucket: "yt-notes-91623.firebasestorage.app",
+  messagingSenderId: "727482074062",
+  appId: "1:727482074062:web:141153934301e8e309a19f",
+  measurementId: "G-S22RVZKX4Z"
 };
 
 const app  = initializeApp(firebaseConfig);
